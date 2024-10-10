e6adb84d9bfe4c2a75cbd1bf8e79a681f7b6a07c
strangeliu <strangeliu@gmail.com>
2024-10-10 10:37:43 +0800

    Initial Commit

---
+++ ViewAliasIssue/ViewAliasIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAliasIssueApp.swift
+//  ViewAliasIssue
+//
+//  Created by Jian Liu on 2024/10/10.
+//
+
+import SwiftUI
+
+@main
+struct ViewAliasIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
